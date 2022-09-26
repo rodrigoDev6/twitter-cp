5cfb403ba24e87b73eb9c86ae1245a8face95071
Rodrigo de Lima Alves <rodrigolima.alves21@gmail.com>
2022-09-25 23:27:57 -0300

    up initial changes

---
+++ app/Http/Livewire/Twettes/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Twettes;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.twettes.index');
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,9 @@
 <x-layouts.app>
-    <h1 class="text-5xl font-bold underline">
-        Hello world!
-    </h1>
+    <div class="grid bg-slate-100 grid-cols-3 w-screen h-screen divide- divide-solid">
+        <div class="flex">
+            01
+        </div>
+        <div>02</div>
+        <div>03</div>
+      </div>
 </x-layouts.app>
