65be8850e888c849d0bc85b7f7bfe43a57fb7ecb
Rodrigo de Lima Alves <rodrigolima.alves21@gmail.com>
2022-09-27 10:13:27 -0300

    up

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -5,39 +5,44 @@
     </div>
     {{-- links --}}
     <ul class="flex flex-col gap-6">
-        <li class="px-2 py-2 antialiased cursor-pointer md:text-base xl:text-2xl hover:rounded-lg hover:bg-gray-200">
+        <li class="px-2 py-2 antialiased cursor-pointer hover:rounded-lg hover:bg-gray-200 md:text-base xl:text-2xl">
             <i class="fa-solid fa-house-chimney"></i>
             <span class="ml-2">Página inicial</span>
         </li>
-        <li class="px-2 py-2 antialiased cursor-pointer md:text-base xl:text-2xl hover:rounded-lg hover:bg-gray-200">
+        <li class="px-2 py-2 antialiased cursor-pointer hover:rounded-lg hover:bg-gray-200 md:text-base xl:text-2xl">
             <i class="fa-solid fa-hashtag"></i>
             <span class="ml-2">Explorar</span>
         </li>
-        <li class="px-2 py-2 antialiased cursor-pointer md:text-base xl:text-2xl hover:rounded-lg hover:bg-gray-200">
+        <li class="px-2 py-2 antialiased cursor-pointer hover:rounded-lg hover:bg-gray-200 md:text-base xl:text-2xl">
             <i class="fa-regular fa-bell"></i>
             <span class="ml-2">Notificações</span>
 
         </li>
-        <li class="px-2 py-2 antialiased cursor-pointer md:text-base xl:text-2xl hover:rounded-lg hover:bg-gray-200">
+        <li class="px-2 py-2 antialiased cursor-pointer hover:rounded-lg hover:bg-gray-200 md:text-base xl:text-2xl">
             <i class="fa-regular fa-envelope"></i>
             <span class="ml-2">Mensagens</span>
         </li>
-        <li class="px-2 py-2 antialiased cursor-pointer md:text-base xl:text-2xl hover:rounded-lg hover:bg-gray-200">
+        <li class="px-2 py-2 antialiased cursor-pointer hover:rounded-lg hover:bg-gray-200 md:text-base xl:text-2xl">
             <i class="fa-regular fa-bookmark"></i>
             <span class="ml-2">Itens Salvos</span>
 
         </li>
-        <li class="px-2 py-2 antialiased cursor-pointer md:text-base xl:text-2xl hover:rounded-lg hover:bg-gray-200">
+        <li class="px-2 py-2 antialiased cursor-pointer hover:rounded-lg hover:bg-gray-200 md:text-base xl:text-2xl">
             <i class="fa-regular fa-file-lines"></i>
             <span class="ml-2">Listas</span>
         </li>
-        <li class="px-2 py-2 antialiased cursor-pointer md:text-base xl:text-2xl hover:rounded-lg hover:bg-gray-200">
+        <li class="px-2 py-2 antialiased cursor-pointer hover:rounded-lg hover:bg-gray-200 md:text-base xl:text-2xl">
             <i class="fa-regular fa-user"></i>
             <span class="ml-2">Perfil</span>
         </li>
-        <li class="px-2 py-2 antialiased cursor-pointer md:text-base xl:text-2xl hover:rounded-lg hover:bg-gray-200">
+        <li class="px-2 py-2 antialiased cursor-pointer hover:rounded-lg hover:bg-gray-200 md:text-base xl:text-2xl">
             <i class="fa-solid fa-ellipsis"></i>
             <span class="ml-2">Mais</span>
         </li>
     </ul>
+    <div class="flex gap-3 align-middle">
+        <span>Nome do usuario</span>
+        <input type="button" value="Sair"class="text-white rounded-full cursor-pointer w-14 bg-rose-500 hover:bg-rose-700" />
+    </div>
 </div>
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
     <div class="grid w-screen h-screen grid-cols-3 bg-slate-100">
         {{-- sidebar menu --}}
         <div class="grid justify-center">
-            <x-sidebar></x-sidebar>
+            <x-sidebar />
         </div>
         {{-- tweets --}}
         <div class="grid">
@@ -15,7 +15,7 @@
 
                 <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
                 
-                
+                <livewire:tweetes.timeline />
             </div>
         </div>
         {{-- endTweets --}}
